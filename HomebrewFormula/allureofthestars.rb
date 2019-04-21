01384c8e8a28d14aceaa15d25fab016e474f8d41
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2019-04-21 18:17:41 +0200

    Update Allure version in formula

--- HomebrewFormula/allureofthestars.rb
+++ HomebrewFormula/allureofthestars.rb
@@ -5,8 +5,8 @@ class Allureofthestars < Formula
 
   desc "Near-future Sci-Fi roguelike and tactical squad combat game"
   homepage "http://allureofthestars.com"
-  url "https://hackage.haskell.org/package/Allure-0.9.3.3/Allure-0.9.3.3.tar.gz"
-  sha256 "22cc64f863bd78b24228175ae338cf13c664728e2291bed3c3dbb3c5aa434e68"
+  url "https://hackage.haskell.org/package/Allure-0.9.4.0/Allure-0.9.4.0.tar.gz"
+  sha256 "503cd08dd6dd71d0afe63920b8fa171047449e95a35369dab0936c490d3dabf4"
   head "https://github.com/AllureOfTheStars/Allure.git"
 
   depends_on "cabal-install" => :build
