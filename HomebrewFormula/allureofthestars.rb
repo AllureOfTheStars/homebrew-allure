38a302efd3775f997f3a4d47ef580868ab789e68
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2019-04-23 13:07:38 +0200

    Make the test stricter by reading config, etc.

--- HomebrewFormula/allureofthestars.rb
+++ HomebrewFormula/allureofthestars.rb
@@ -19,7 +19,7 @@ class Allureofthestars < Formula
   end
 
   test do
-    output = shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli --logPriority 0 --newGame 3 --maxFps 100000 --benchmark --stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle --setDungeonRng 7 --setMainRng 7")
+    output = shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli --logPriority 0 --newGame 3 --maxFps 100000 --stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle --setDungeonRng 7 --setMainRng 7")
     assert_equal "", output
     assert_equal "", shell_output("cat ~/.Allure/stderr.txt")
     assert_match "UI client FactionId 1 stopped", shell_output("cat ~/.Allure/stdout.txt")
