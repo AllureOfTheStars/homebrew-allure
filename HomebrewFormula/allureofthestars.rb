129b99d9039d1c0a1d005dbd06fe64aa35a941c8
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2020-03-10 22:07:09 +0100

    Shorten a line to satisfy Brew or Ruby

--- HomebrewFormula/allureofthestars.rb
+++ HomebrewFormula/allureofthestars.rb
@@ -19,8 +19,9 @@ class Allureofthestars < Formula
   end
 
   test do
-    non_debug_args = "--logPriority 0 --newGame 3 --maxFps 100000 --stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle --setDungeonRng 7 --setMainRng 7"
-    output = shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli #{non_debug_args}")
+    play_args = "--maxFps 100000 --stopAfterFrames 50 --automateAll --keepAutomated"
+    misc_args = "--logPriority 0 --newGame 3 --gameMode battle --setDungeonRng 7 --setMainRng 7"
+    output = shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli #{play_args} #{misc_args}")
     assert_equal "", output
     assert_equal "", shell_output("cat ~/.Allure/stderr.txt")
     assert_match "UI client FactionId 1 stopped", shell_output("cat ~/.Allure/stdout.txt")
