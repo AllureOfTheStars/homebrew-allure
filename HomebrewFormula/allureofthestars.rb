8cd7e56443c60334c8a13d7bfbc388f97417425f
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-04-06 16:44:24 +0200

    Updated formula for v0.10.2.0, except for sha256

--- HomebrewFormula/allureofthestars.rb
+++ HomebrewFormula/allureofthestars.rb
@@ -5,8 +5,8 @@ class Allureofthestars < Formula
 
   desc "Near-future Sci-Fi roguelike and tactical squad combat game"
   homepage "https://www.allureofthestars.com/"
-  url "https://hackage.haskell.org/package/Allure-0.9.5.0/Allure-0.9.5.0.tar.gz"
-  sha256 "8180fe070633bfa5515de8f7443421044e7ad4ee050f0a92c048cec5f2c88132"
+  url "https://hackage.haskell.org/package/Allure-0.10.2.0/Allure-0.10.2.0.tar.gz"
+  sha256 ""
   head "https://github.com/AllureOfTheStars/Allure.git"
 
   depends_on "cabal-install" => :build
@@ -19,10 +19,11 @@ class Allureofthestars < Formula
   end
 
   test do
-    non_debug_args = "--logPriority 0 --newGame 3 --maxFps 100000 --stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle --setDungeonRng 7 --setMainRng 7"
-    output = shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli #{non_debug_args}")
+    play_args = "--maxFps 100000 --stopAfterFrames 50 --automateAll --keepAutomated"
+    misc_args = "--logPriority 0 --newGame 3 --gameMode battle --setDungeonRng \"SMGen 7 7\" --setMainRng \"SMGen 7 7\""
+    output = shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli #{play_args} #{misc_args}")
     assert_equal "", output
     assert_equal "", shell_output("cat ~/.Allure/stderr.txt")
-    assert_match "UI client FactionId 1 stopped", shell_output("cat ~/.Allure/stdout.txt")
+    assert_match "Client FactionId 1 closed frontend.", shell_output("cat ~/.Allure/stdout.txt")
   end
 end
